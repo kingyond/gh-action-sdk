4a1d5cbd5b349a511eb73d797620af39839260e3
Kevin <beijiu572@gmail.com>
2021-06-26 11:38:49 +0800

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ cd /home/build/openwrt/
 echo "dir $CUSTOM_PKG_DIR"
 cp -r /github/workspace "package/$CUSTOM_PKG_DIR"
 ls "package/$CUSTOM_PKG_DIR"
+ls -R "package/$CUSTOM_PKG_DIR"
 echo "ahhhh"
 
 ls -R
