94e5e048af2f9d5c98538e5b14a352f41cda1c6a
cielpy <beijiu572@gmail.com>
2021-06-26 11:28:18 +0800

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,6 @@ set -ef
 cd /home/build/openwrt/
 cp -r /github/workspace "package/$CUSTOM_PKG_DIR"
 ls "package/$CUSTOM_PKG_DIR"
-./scripts/feeds update -a
-make defconfig
-./scripts/feeds install -a
 
 
-make package/$PACKAGES/{clean,compile} -j1 V=s
-
-if [ -d bin/ ]; then
-	ls -R bin/
-	mv bin/ "$GITHUB_WORKSPACE/"
-fi
+ls -R package/
