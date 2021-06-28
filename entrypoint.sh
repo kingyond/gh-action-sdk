5077d55c90e5661e5f5a4474b506b3b7a60369c6
Kevin <beijiu572@gmail.com>
2021-06-28 11:03:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 set -ef
+
+cat feeds.conf.default >> feeds.conf
+
+#shellcheck disable=SC2153
+for EXTRA_FEED in $EXTRA_FEEDS; do
+	echo "$EXTRA_FEED" | tr '|' ' ' >> feeds.conf
+done
+cat feeds.conf
+
 cd /home/build/openwrt/
 cp -r /github/workspace "package/$CUSTOM_PKG_DIR"
 ls "package/$CUSTOM_PKG_DIR"
