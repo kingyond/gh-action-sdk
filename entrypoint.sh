232148f1ad0655521b76492d105a172f940a199f
cielpy <beijiu572@gmail.com>
2021-06-28 16:17:16 +0800

    feat : support make index

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,21 +20,36 @@ cat feeds.conf
 ./scripts/feeds update -a > /dev/null
 make defconfig > /dev/null
 
+
 if [ -z "$FEEDS_NEED_INSTALL" ]; then
-	./scripts/feeds install -a > /dev/null
+	./scripts/feeds install -d y -f -a
 else
 	for FEED in $FEEDS_NEED_INSTALL; do
-		./scripts/feeds install -a -p $FEED > /dev/null
+		./scripts/feeds install -a -p $FEED -f > /dev/null
 	done
 fi
-
 
 cp -r /github/workspace "package/$CUSTOM_PKG_DIR"
 ls "package/$CUSTOM_PKG_DIR"
 
-for pkg in $PACKAGES; do
-	make package/$pkg/compile -j1 V=s
-done
+
+if [ -z "$PACKAGES" ]; then
+	# compile all packages in feed
+	make \
+		-j "$(nproc)" \
+		V=s
+
+else
+	for pkg in $PACKAGES; do
+		make package/$pkg/compile \
+			-j "$(nproc)" \
+			V=s
+	done
+fi
+
+if [ "INDEX" = "1" ];then
+	make package/index
+fi
 
 
 if [ -d bin/ ]; then
